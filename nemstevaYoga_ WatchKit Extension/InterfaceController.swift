9d0abea7179595c6cc71f529e88406c6285ae14b
NemcevaPolina <createmylife678@gmail.com>
2022-01-31 16:08:07 +0300

    сообщение

--- nemstevaYoga_ WatchKit Extension/InterfaceController.swift
+++ nemstevaYoga_ WatchKit Extension/InterfaceController.swift
@@ -7,14 +7,34 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
-
-class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
-        // Configure interface objects here.
+class InterfaceController: WKInterfaceController, WCSessionDelegate{
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
     }
     
+   
+    var wcSession: WCSession!
+    
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var image: WKInterfaceImage!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    
+
+    override func awake(withContext context: Any?) {
+        wcSession = WCSession.default
+        wcSession.delegate = self
+        wcSession.activate()
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String: Any]) {
+        let message = applicationContext["name"] as? String
+        let time = applicationContext["time"] as? Double
+        name.setText(message)
+        timer.setDate(NSDate(timeIntervalSinceNow: time!) as Date)
+        timer.start()
+    }
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
     }
